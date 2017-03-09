10bbb65cb9b6e0ea72086aaacfd6ecf26a1d0198
rebelpixeldev <tsczelecki@konradgroup.com>
2017-03-09 08:52:46 -0500

    Base layout setup

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import { Topbar } from 'amped/modules/Topbar'
+import {Topbar, Sidebar} from 'amped/modules/Layout'
 import './CoreLayout.scss'
 import '../../styles/core.scss'
 
-export const CoreLayout = ({ children }) => (
-  <div className='container text-center'>
-      <Topbar></Topbar>
-    <div className='core-layout__viewport'>
-      {children}
-    </div>
-  </div>
+export const CoreLayout = ({children}) => (
+	<div className='amped-container'>
+		<Topbar />
+		<div className="amped-content-container">
+			<Sidebar />
+			<div className='amped-content'>
+				{children}
+			</div>
+		</div>
+	</div>
 )
 
 CoreLayout.propTypes = {
-  children : React.PropTypes.element.isRequired
+	children: React.PropTypes.element.isRequired
 }
 
 export default CoreLayout
